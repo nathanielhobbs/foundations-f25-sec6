44ac7d8b8f4569bc84daa9b82bef3224e489e9f1
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2025-11-12 13:28:09 -0500

    command line args (sys vs argparse), random, script vs modules

---
+++ random_example.py
@@ -0,0 +1,5 @@
+import random
+
+print(f'random number between 0 and 1: {random.random()=}')
+print(f'random integer between 0 and 100: {random.randint(0,100)=}')
+print(f'random element from list: {random.choice(['a','b','c'])=}')
